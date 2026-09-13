bcb5731a958eb79e6a6fb0842e58b20d24447c7c
Vaidehi Koranne <korannevaidehi@gmail.com>
2026-09-13 12:36:16 +0530

    Prepare React frontend for production

--- frontend/navsmfs-frontend/src/components/Scanner.tsx
+++ frontend/navsmfs-frontend/src/components/Scanner.tsx
@@ -180,7 +180,7 @@ export default function Scanner() {
 
       const response =
         await fetch(
-          'http://127.0.0.1:8000/api/analyze/',
+          '/api/analyze/',
           {
             method: 'POST',
 
